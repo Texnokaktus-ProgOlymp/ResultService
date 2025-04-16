85d26cd878f0d1ca008f990cafac05a1aa6c6389
Artem Krylov <kav128@outlook.com>
2025-04-16 12:09:11 +0300

    add Serilog

--- Texnokaktus.ProgOlymp.ResultService/Program.cs
+++ Texnokaktus.ProgOlymp.ResultService/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using Texnokaktus.ProgOlymp.ResultService.DataAccess;
 using Texnokaktus.ProgOlymp.ResultService.Services;
 
@@ -8,6 +9,8 @@ builder.Services.AddDataAccess(optionsBuilder => optionsBuilder.UseSqlServer(bui
                                                                .EnableSensitiveDataLogging(builder.Environment.IsDevelopment()));
 
 builder.Services.AddGrpc();
+
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
 
