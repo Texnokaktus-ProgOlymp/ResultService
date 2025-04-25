22505b4608f7bca0640f7eb4103cecbd10fcedd5
Artem Krylov <kav128@outlook.com>
2025-04-25 20:41:24 +0300

    add ef migration

---
+++ Texnokaktus.ProgOlymp.ResultService.DataAccess/Migrations/20250425174027_Initial.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Texnokaktus.ProgOlymp.ResultService.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContestResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContestId = table.Column<int>(type: "int", nullable: false),
+                    Stage = table.Column<int>(type: "int", nullable: false),
+                    StageId = table.Column<long>(type: "bigint", nullable: false),
+                    Published = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContestResults", x => x.Id);
+                    table.UniqueConstraint("AK_ContestResults_ContestId_Stage", x => new { x.ContestId, x.Stage });
+                    table.UniqueConstraint("AK_ContestResults_StageId", x => x.StageId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Problems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContestResultId = table.Column<int>(type: "int", nullable: false),
+                    Alias = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Problems", x => x.Id);
+                    table.UniqueConstraint("AK_Problems_ContestResultId_Alias", x => new { x.ContestResultId, x.Alias });
+                    table.ForeignKey(
+                        name: "FK_Problems_ContestResults_ContestResultId",
+                        column: x => x.ContestResultId,
+                        principalTable: "ContestResults",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProblemResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProblemId = table.Column<int>(type: "int", nullable: false),
+                    ParticipantId = table.Column<int>(type: "int", nullable: false),
+                    BaseScore = table.Column<decimal>(type: "decimal(5,2)", precision: 5, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProblemResults", x => x.Id);
+                    table.UniqueConstraint("AK_ProblemResults_ProblemId_ParticipantId", x => new { x.ProblemId, x.ParticipantId });
+                    table.ForeignKey(
+                        name: "FK_ProblemResults_Problems_ProblemId",
+                        column: x => x.ProblemId,
+                        principalTable: "Problems",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ScoreAdjustments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProblemResultId = table.Column<int>(type: "int", nullable: false),
+                    Adjustment = table.Column<decimal>(type: "decimal(5,2)", precision: 5, scale: 2, nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ScoreAdjustments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ScoreAdjustments_ProblemResults_ProblemResultId",
+                        column: x => x.ProblemResultId,
+                        principalTable: "ProblemResults",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ScoreAdjustments_ProblemResultId",
+                table: "ScoreAdjustments",
+                column: "ProblemResultId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ScoreAdjustments");
+
+            migrationBuilder.DropTable(
+                name: "ProblemResults");
+
+            migrationBuilder.DropTable(
+                name: "Problems");
+
+            migrationBuilder.DropTable(
+                name: "ContestResults");
+        }
+    }
+}
